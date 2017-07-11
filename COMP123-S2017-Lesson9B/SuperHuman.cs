c335cbb2d0a5d7dbc2ac6d76352cde56acb75d11
300794883 <300794883@PR-A315-30>
2017-07-11 17:47:23 -0400

    Added initialize method to SuperHuman class

--- COMP123-S2017-Lesson9B/SuperHuman.cs
+++ COMP123-S2017-Lesson9B/SuperHuman.cs
@@ -7,7 +7,7 @@ using System.Text;
  * Name: Zilang Liang
  * Date: July 11, 2017
  * Description: SuperHuman Class that extends the HUman Abstract class
- * Version: 0.1 - Created the SuperHuman class
+ * Version: 0.2 - Added _initialize method
 */
 
 namespace COMP123_S2017_Lesson9B
@@ -18,20 +18,31 @@ namespace COMP123_S2017_Lesson9B
     public class SuperHuman : Human
     {
         //PRIVATE INSTANCE VARIABLES
-
+        private List<Power> _powers;
         //PUBLIC PROPERTIES
 
         //CONSTRUCTOR
 
-
+        /// <summary>
+        /// this is the main constructor for the SuperHuman class.
+        /// It takes one parameter - name(string)
+        /// </summary>
+        /// <param name="name"></param>
         public SuperHuman(string name)
             : base(name)
         {
-
+            this._initialize();
         }
 
 
         //PRIVATE METHODS
+        /// <summary>
+        /// This methods initializes, instantiates and ssigns value to class properties.
+        /// </summary>
+        private void _initialize()
+        {
+            this._powers = new List<Power>();//creates an empty list
+        }
 
         //PUBLIC METHODS
     }
